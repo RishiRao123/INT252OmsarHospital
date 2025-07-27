497157bd3c30e3fdcf5ab4b586ee300c9b16ed5a
Rishi Rao <rishiraopuskuri@gmail.com>
2025-07-28 00:09:46 +0530

    UI update in frontend and backend layout

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 
+
+//Banner component
 const Banner = () => {
   const navigate = useNavigate();
 
