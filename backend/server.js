653ff7bacb352d2207e84076d9010d306d6bd380
Rishi Rao <rishiraopuskuri@gmail.com>
2025-08-20 00:31:48 +0530

    fix: stable CORS config for vercel apps

--- backend/server.js
+++ backend/server.js
@@ -15,16 +15,20 @@ connectCloudinary();
 
 // middlewares
 app.use(express.json());
-app.use(cors({
-  origin: [
-    "https://omsar-hospital-61bhc61fw-rishi-raos-projects-78d70f55.vercel.app/",
-    "https://omsar-hospital-admin-nw4xfn90g-rishi-raos-projects-78d70f55.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
-
+app.use(
+  cors({
+    origin: [
+      "https://omsar-hospital.vercel.app",
+      "https://omsar-hospital-admin.vercel.app",
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 // api endpoints
 app.use("/api/user", userRouter);
